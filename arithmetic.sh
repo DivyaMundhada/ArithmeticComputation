17e41bd683405a68a856231c9c14763bbd06b3e0
DivyaMundhada <divyamundada9@gmail.com>
2021-05-25 14:56:16 +0530

    Added Arithmetic Computation and stored in Dictionary feature

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,7 +1,12 @@
 #! /bin/bash -x
 
+declare -A dict
+
 echo "Enter any three numbers:"
 read a b c
 echo The three numbers are $a,$b,$c
-result=`expr $(( $a % $b )) + $c `
-echo $result
+dict['op1']=`expr  $a + $(( $b * $c )) `
+dict['op2']=`expr  $(( $a * $b )) + $c `
+dict['op3']=`expr $c + $(( $a / $b )) `
+dict['op4']=`expr $(( $a % $b )) + $c `
+echo Result for various operations are: ${dict[*]}
